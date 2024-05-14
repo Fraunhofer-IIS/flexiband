e7aea6dfa7f6d68d8306dd8f3b2ef68843bba30c
Fabio Garzia <fabio.garzia@iis.fraunhofer.de>
2024-05-14 13:45:56 +0200

    driver: linux: Add GOOSE udev rules

--- driver/linux/install.sh
+++ driver/linux/install.sh
@@ -66,8 +66,10 @@ fi
 mkdir -p ${TELEORBIT_IMG_PATH}
 cp flexiband.img ${TELEORBIT_IMG_PATH}/flexiband.img
 cp teleorbit.img ${TELEORBIT_IMG_PATH}/teleorbit.img
+cp goose2.img ${TELEORBIT_IMG_PATH}/goose2.img
 cp 80-flexiband.rules /etc/udev/rules.d/80-flexiband.rules
 cp 80-teleorbit.rules /etc/udev/rules.d/80-teleorbit.rules
+cp 80-goose-rec-rep.rules /etc/udev/rules.d/80-goose-rec-rep.rules
 cp src/fwload_fx3 ${TELEORBIT_BIN_PATH}/fwload_fx3
 cp upload_fx3.sh ${TELEORBIT_BIN_PATH}/upload_fx3.sh
 cp src/libcyusb.so ${TELEORBIT_LIB_PATH}/libcyusb.so
