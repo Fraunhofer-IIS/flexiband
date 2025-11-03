e2565d867bcded46d7dc4d94cff52eb4a194d007
Fabian Benschuh <Fabian.Benschuh@iis.fraunhofer.de>
2025-11-03 20:00:43 +0100

    Enable multiple device upload
    If multiple deivces are connected the software asks for the correct one
    if none is specified. Otherwise it behaves like before.
    
    Change-Id: I84e3c6540fae11cb79049be843507fe3ea18a359

--- driver/unix/src/fwload_fx3.c
+++ driver/unix/src/fwload_fx3.c
@@ -21,34 +21,28 @@
 
 /********** Cut and paste the following & modify as required  **********/
 static const char         *program_name;
-static const char *const   short_options  = "hvf:e:";
-static const struct option long_options[] = {{"help", 0, NULL, 'h'},
-                                             {"version", 0, NULL, 'v'},
-                                             {"file", 1, NULL, 'f'},
-                                             {NULL, 0, NULL, 0}};
+static const char *const   short_options  = "hvf:d:b:";
+static const struct option long_options[] = {{"help", 0, NULL, 'h'}, {"version", 0, NULL, 'v'},
+                                             {"file", 1, NULL, 'f'}, {"device", 1, NULL, 'd'},
+                                             {"bus", 1, NULL, 'b'},  {NULL, 0, NULL, 0}};
 
 static int next_option;
 
 static void print_usage(FILE *stream, int exit_code)
 {
-    fprintf(stream, "Usage: %s options\n", program_name);
+    fprintf(stream, "Usage: %s [options] [filename]\n", program_name);
     fprintf(stream, "  -h  --help      Display this usage information.\n"
                     "  -v  --version   Print version.\n"
-                    "  -f  --file      firmware file name (.hex) format\n");
+                    "  -f  --file      firmware file name (.hex) format\n"
+                    "  -b  --bus       Bus number of target device.\n"
+                    "  -d  --dev       Device number of target device.\n");
     exit(exit_code);
 }
 /***********************************************************************/
 
-static int   filename_provided;
-static char *filename;
-
-static void validate_inputs(void)
-{
-    if (!filename_provided) {
-        fprintf(stderr, "Please provide full path to firmware image file\n");
-        print_usage(stdout, 1);
-    }
-}
+static char *filename = NULL;
+static int   busnum   = -1;
+static int   devnum   = -1;
 
 int main(int argc, char **argv)
 {
@@ -74,29 +68,42 @@ int main(int argc, char **argv)
     while ((next_option = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
         switch (next_option) {
         case 'h':  // -h or --help
-            print_usage(stdout, 0);
+            print_usage(stdout, EXIT_SUCCESS);
+            // does not return
         case 'v':  // -v or --version
             printf("%s (Ver 1.0)\n", program_name);
             printf("Copyright (C) 2012 Cypress Semiconductors Inc. / ATR-LABS\n");
             exit(0);
         case 'f':  // -f or --file
             filename = optarg;
-            fp       = fopen(filename, "r");
-            if (!fp) {
-                fprintf(stderr, "File not found!\n");
-                print_usage(stdout, 1);
-            }
-            filename_provided = 1;
-            fclose(fp);
+            break;
+        case 'b':  // -b or -bus
+            busnum = atoi(optarg);
+            break;
+        case 'd':  // -d or -device
+            devnum = atoi(optarg);
             break;
         case '?':  // Invalid option
-            print_usage(stdout, 1);
+            print_usage(stdout, EXIT_FAILURE);
+            // does not return
         default:  // Something else, unexpected
             abort();
         }
     }
 
-    validate_inputs();
+    if (optind < argc) { filename = argv[optind]; }
+
+    if (filename == NULL) {
+        fprintf(stderr, "Please provide full path to firmware image file\n");
+        print_usage(stdout, EXIT_FAILURE);
+    }
+
+    fp = fopen(filename, "r");
+    if (!fp) {
+        fprintf(stderr, "File not found!\n");
+        print_usage(stdout, EXIT_FAILURE);
+    }
+    fclose(fp);
 
     r = cyusb_open();
     if (r < 0) {
@@ -105,14 +112,43 @@ int main(int argc, char **argv)
     } else if (r == 0) {
         printf("No device found\n");
         return 0;
-    } else if (r > 1) {
-        printf("Only 1 device supported in this example\n");
-        return 0;
+    } else if (r == 1) {
+        // found only one device (legacy behaviour)
+        h = cyusb_gethandle(0);
+        r = stat(filename, &statbuf);
+        printf("File size = %d\n", (int)statbuf.st_size);
+        r = cyusb_download_fx3(h, filename);
+
+    } else if (busnum != -1 & devnum != -1) {
+        // search for specific device
+        for (i = 0; i < r; i++) {
+            h = cyusb_gethandle(i);
+            if (busnum == cyusb_get_busnumber(h) && devnum == cyusb_get_devaddr(h)) {
+                r = stat(filename, &statbuf);
+                printf("File size = %d\n", (int)statbuf.st_size);
+                r = cyusb_download_fx3(h, filename);
+                break;
+            }
+        }
+    } else {
+        // prompt user to choose device
+        printf("Enumerating %d devices...\n", r);
+        for (i = 0; i < r; i++) {
+            h                   = cyusb_gethandle(i);
+            unsigned int pid    = cyusb_getproduct(h);
+            unsigned int vid    = cyusb_getvendor(h);
+            unsigned int busnum = cyusb_get_busnumber(h);
+            unsigned int devnum = cyusb_get_devaddr(h);
+            printf("[%i] vid = %x, pid = %x @ bus %d dev %d\n", i, vid, pid, busnum, devnum);
+        }
+        printf("Choose device: ");
+        scanf("%d", &i);
+        h = cyusb_gethandle(i);
+        r = stat(filename, &statbuf);
+        printf("\nFile size = %d\n", (int)statbuf.st_size);
+        r = cyusb_download_fx3(h, filename);
     }
-    h = cyusb_gethandle(0);
-    r = stat(filename, &statbuf);
-    printf("File size = %d\n", (int)statbuf.st_size);
-    r = cyusb_download_fx3(h, filename);
+
     cyusb_close();
     close(fd);
     return 0;
--- driver/unix/upload_fx3.sh
+++ driver/unix/upload_fx3.sh
@@ -4,7 +4,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     export DYLD_LIBRARY_PATH=/usr/local/lib/:/local/lib/:$DYLD_LIBRARY_PATH
 else
+    # Linux
     export LD_LIBRARY_PATH=/usr/local/lib/:/local/lib/:$LD_LIBRARY_PATH
 fi
 
-/usr/local/bin/fwload_fx3 -f $1
+/usr/local/bin/fwload_fx3 $@
